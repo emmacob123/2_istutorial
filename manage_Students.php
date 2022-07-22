467911c8ba5131b6acdd5cdb222e1596a8f30461
emmacob123 <88585192+emmacob123@users.noreply.github.com>
2022-07-22 19:26:58 +0100

    2_istutorial
    Edit all

--- manage_Students.php
+++ manage_Students.php
@@ -14,7 +14,7 @@
 
     
 
-<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="assets/css/bootstrap.css" rel="stylesheet">
 
     <style>
       .bd-placeholder-img {
@@ -100,13 +100,13 @@
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="user_mng.php">
               <span data-feather="file" class="align-text-bottom"></span>
               Manage Users
             </a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">
+            <a class="nav-link" href="manageorder.php">
               <span data-feather="shopping-cart" class="align-text-bottom"></span>
               Manage Orders
             </a>
@@ -358,7 +358,7 @@
 </div>
 
 
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 
       <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="dashboard.js"></script>
   </body>
